46cd6e6ec5cf7f2f70dfc7850b595e737ee26b8e
George Mauer <gmauer@gmail.com>
2013-01-12 18:26:48 -0600

    pulling in some real data yo

--- allinone/js/home.js
+++ allinone/js/home.js
@@ -45,9 +45,12 @@ function initFilters() {
 	return $('#filters-area select');
 }
 
-function filterRanking(ranking) { return function(ev) {
-	return !ranking || ev.ranking >= ranking;
-}}
+function filterRanking(ranking) { 
+	ranking = +ranking;
+	return function(ev) {
+		return !ranking || ev.ranking >= ranking;
+	}
+}
 function filterDay(daytime) { 
 	var day = new Date(parseInt(daytime, 10)).getDay();
 	return function(ev) { 
@@ -90,30 +93,37 @@ function initToggles() {
 ///////////////////////////////////////////////////
 
 function gettingEvents() {
-	return $.Deferred(function(d){
-		d.resolve( _.map(_.range(35), function(i) {
-			return {
-				 eventName: "Here is some event "+i
-				,venue: "Venue "+_.random(10)
-				,location: null
-				,time: new Date(1358014168714).add({hours: _.random(72)}).getTime()
-				,image: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRSHHZb6Dt0Sssbz0nzT-MUvgwmtf11T2DzVkDC1ONsO2z62num'
-				,price: null
-				,description: "<p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.  </p>"
-				,source: '<a href="http://cheezburger.com/6924526080">This guy\'s blog</a>'
-				,ranking: _.random(1)||null
-				,links: [
-					{
-					 	 type: ['music', 'gcal', 'info'][_.random(2)]
-					 	,text: "Text "+i
-						,link: 'https://play.google.com/music/listen?u=1'
-					}
-				]
-			};
-		}) );
-	});
-}
+	return $.getJSON('/events.json').pipe(stubAmmendEvents);
 
+	//  $.Deferred(function(d){
+	// 	d.resolve( _.map(_.range(35), function(i) {
+	// 		return {
+	// 			 eventName: "Here is some event "+i
+	// 			,venue: "Venue "+_.random(10)
+	// 			,location: null
+	// 			,time: new Date(1358014168714).add({hours: _.random(72)}).getTime()
+	// 			,image: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRSHHZb6Dt0Sssbz0nzT-MUvgwmtf11T2DzVkDC1ONsO2z62num'
+	// 			,price: null
+	// 			,description: "<p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.  </p>"
+	// 			,source: '<a href="http://cheezburger.com/6924526080">This guy\'s blog</a>'
+	// 			,ranking: _.random(1)||null
+	// 			,links: [
+	// 				{
+	// 				 	 type: ['music', 'gcal', 'info'][_.random(2)]
+	// 				 	,text: "Text "+i
+	// 					,link: 'https://play.google.com/music/listen?u=1'
+	// 				}
+	// 			]
+	// 		};
+	// 	}) );
+	// });
+}
+function stubAmmendEvents(allEvents){
+	return _.map(allEvents, function(ev) {
+		ev.time = new Date().add({hours: _.random(72)}).getTime();
+		return ev;
+	})
+}
 ///////////////////////////////////////////////////
 
 function pageInitializing() {
@@ -124,15 +134,16 @@ function pageInitializing() {
 
 ///////////////////////////////////////////////////
 function extendHandlebars() {
-	Handlebars.registerHelper('html', function(html) {
+	Handlebars.registerHelper('html', truthyOr('', function(html) {
 	  return new Handlebars.SafeString(html);
-	});
+	}));
 	Handlebars.registerHelper('time', truthyOr('', function(timestamp) {
 	  return !timestamp ? '' : new Date(timestamp).toFormat('H:MM PP');
 	}));
-	Handlebars.registerHelper('eventImage', truthyOr('', function() {
-	  return new Handlebars.SafeString('<img src="'+this.image+'" alt="'+this.eventName+'" class="event-image"/>');
-	}));
+	Handlebars.registerHelper('eventImage', function() {
+		if(!this.image) return '';
+	  	return new Handlebars.SafeString('<img src="'+this.image.src+'" alt="'+this.eventName+'" class="event-image"/>');
+	});
 	Handlebars.registerHelper('eventLink', truthyOr('', function(link) {
 	  return new Handlebars.SafeString('<a href="'+link.link+'"><span class="icon '+link.type+'"></span><span class="link-name">'+link.text+'</span></a>');
 	}));
